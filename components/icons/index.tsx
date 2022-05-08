1909950cd7e49f6845dcef23c4f0e007c3110459
callmehalpha <hello@callmehalpha.me>
2022-05-08 12:46:07 +0100

    removed all mods to empty project

--- components/icons/index.tsx
+++
@@ -1,3 +0,0 @@
-export {default as CheckIcon} from './CheckIcon';
-export {default as ChevronDownIcon} from './ChevronDown';
-export {NetFuseLogo} from './NetFuseLogo';
--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+  reactStrictMode: true,
 }
-const withPWA = require('next-pwa')
 
 module.exports = nextConfig
-
-module.exports = withPWA({
-    pwa: {
-        dest: 'public'
-    }
-})
