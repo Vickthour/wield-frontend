44f7c237abb8ca554615990e5acd95ff921e3cc7
Code-Victor <oluwaborihamzat@gmail.com>
2022-06-24 14:18:49 +0100

    created icon button with text and added smaller size variants

--- components/base/IconButton.tsx
+++ components/base/IconButton.tsx
@@ -1,8 +1,8 @@
 import { variantProps, VariantPropsOf } from "classname-variants/react";
-import React, { FC, HTMLAttributes, ReactNode } from "react";
+import React, { FC, HTMLAttributes, ReactNode, useState } from "react";
 import { EmailIcon } from "../icons";
 
-const tw= String.raw
+const tw = String.raw;
 
 interface IconButtonProps {
   icon: ReactNode;
@@ -13,7 +13,8 @@ const IconClassNames = variantProps({
   base: "grid origin-center place-items-center rounded-full active:scale-95",
   variants: {
     size: {
-      sm: "h-8 w-8",
+      xs: "h-7 w-7",
+      sm: "h-9 w-9",
       md: " h-12 w-12",
       lg: "h-14 w-14",
     },
@@ -23,33 +24,70 @@ const IconClassNames = variantProps({
     color: {
       primary: tw`bg-primaryColor`,
       pink: tw`bg-[#FF685B0F]`,
-      white: 'bg-white',
-      custom: '',
+      white: tw`bg-white hover:bg-gray-50 active:bg-gray-100`,
+      gray: tw`bg-gray-100`,
+      custom: "",
     },
-    border:{
-      white:tw`border border-white`,
-      none:'border-none'
-    }
+    border: {
+      white: tw`border border-white`,
+      none: "border-none",
+    },
   },
-  defaultVariants:{
-    size:'lg',
-    shadow:false,
-    border:'none',
-    color:'custom'
-  }
+  defaultVariants: {
+    size: "lg",
+    shadow: false,
+    border: "none",
+    color: "custom",
+  },
 });
-type IconButtonExtProps=JSX.IntrinsicElements["button"] &
-  VariantPropsOf<typeof IconClassNames> & IconButtonProps;
-const IconButton: FC<IconButtonExtProps> = ({ icon, onClick,...props }) => {
+type IconButtonExtProps = JSX.IntrinsicElements["button"] &
+  VariantPropsOf<typeof IconClassNames> &
+  IconButtonProps;
+const IconButton = ({ icon, onClick, ...props }: IconButtonExtProps) => {
   return (
-    <button
-      onClick={onClick}
-      type="button"
-      {...IconClassNames(props)}
-    >
+    <button onClick={onClick} type="button" {...IconClassNames(props)}>
       {icon}
     </button>
   );
 };
-
+type IcontextProps = JSX.IntrinsicElements["button"] & {
+  text: string;
+  icon: ReactNode;
+  activeIcon?: ReactNode;
+};
+const IconText = ({
+  icon,
+  activeIcon,
+  text,
+  onClick,
+  ...otherProps
+}: IcontextProps) => {
+  const [clicked, setClicked] = useState(false);
+  if (activeIcon) {
+    return (
+      <button
+        className="flex h-full flex-col items-center justify-between"
+        onClick={() => {
+          setClicked(!clicked);
+          onClick;
+        }}
+        {...otherProps}
+      >
+        {clicked ? activeIcon : icon}
+        <h1 className="text-sm">{text}</h1>
+      </button>
+    );
+  }
+  return (
+    <button
+      className="flex flex-col items-center"
+      onClick={onClick}
+      {...otherProps}
+    >
+      {icon}
+      <h1 className="text-sm">{text}</h1>
+    </button>
+  );
+};
+IconButton.text = IconText;
 export default IconButton;
