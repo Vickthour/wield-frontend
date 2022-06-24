09642dbf47ea19fad6f75fbd06dc8b993ba20f9d
Code-Victor <oluwaborihamzat@gmail.com>
2022-06-24 14:24:40 +0100

    added post input and posts

--- components/inc/MainPane.tsx
+++ components/inc/MainPane.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import Post from './Post'
-import StoryPane from './StoryPane'
+import React from "react";
+import { Card} from "../base";
+import Post from "./Post";
+import StoryPane from "./StoryPane";
 
 const MainPane = () => {
   return (
-    <div className=' sm:col-span-6 space-y-4'>
-      <StoryPane/>
-      <Post/>
+    <div className=" space-y-2 sm:col-span-6 mid:space-y-4">
+      <StoryPane />
+      <Card>
+        <label htmlFor="NewPost" className="text-lg font-semibold">
+          New Post
+        </label>
+        <input
+          id="NewPost"
+          type={"text"}
+          placeholder="What are you posting today, James?"
+          className="h-10 w-full rounded-full bg-gray-200   pl-4"
+        />
+      </Card>
+      <Post />
+      <Post />
     </div>
-  )
-}
+  );
+};
 
-export default MainPane
+export default MainPane;
