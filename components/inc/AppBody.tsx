ab335442a0693a42dd7f204b19e6bad9cfd389eb
Code-Victor <oluwaborihamzat@gmail.com>
2022-07-05 17:27:54 +0100

    made more reusuable

--- components/inc/AppBody.tsx
+++ components/inc/AppBody.tsx
@@ -4,12 +4,25 @@ import MainPane from "./MainPane";
 import RightPane from "./RightPane";
 import SideNav from "./SideNav";
 
-const AppBody = ({children}:{children:React.ReactNode}) => {
+const AppBody = ({
+  children,
+  grid=false,
+}: {
+  children: React.ReactNode;
+  grid?: boolean;
+}) => {
+  if (grid) {
+    return (
+      <main className="mt-20 mb-16 min-h-[calc(100vh_-_80px)] bg-background md:pt-5 mid:mb-0 ">
+        <div className=" mx-auto grid grid-cols-1 gap-3 sm:px-1 md:grid-cols-9 lg:grid-cols-12 xl:max-w-[1536px]">
+          {children}
+        </div>
+      </main>
+    );
+  }
   return (
-    <main className="mt-20 min-h-[calc(100vh_-_80px)] bg-background md:pt-5 mb-16 mid:mb-0 ">
-      <div className=" mx-auto grid grid-cols-1 gap-3 sm:px-1 md:grid-cols-9 lg:grid-cols-12 xl:max-w-[1536px]">
-        {children}
-      </div>
+    <main className="mt-20 mb-16 min-h-[calc(100vh_-_80px)] bg-background md:pt-5 mid:mb-0 ">
+      {children}
     </main>
   );
 };
