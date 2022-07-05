b337448c6ec9215020ea0dce4759eb04f10a4843
Code-Victor <oluwaborihamzat@gmail.com>
2022-07-05 17:22:24 +0100

    some layout adjustments

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,5 +1,11 @@
+import {
+  motion,
+  useSpring,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
 import Head from "next/head";
-import React from "react";
+import React, { useRef } from "react";
 import { Card } from "../../components/base";
 import AppBody from "../../components/inc/AppBody";
 import AppNav from "../../components/inc/AppNav";
@@ -8,13 +14,15 @@ import LeftPane from "../../components/inc/LeftPane";
 import MainPane from "../../components/inc/MainPane";
 import Post from "../../components/inc/Post";
 import RightPane from "../../components/inc/RightPane";
+import StoryModal from "../../components/inc/StoryModal";
 import StoryPane from "../../components/inc/StoryPane";
 
 const app = () => {
   return (
     <>
+      <ProgressIndicator />
       <AppNav />
-      <AppBody>
+      <AppBody grid>
         <LeftPane />
         <MainPane>
           <StoryPane />
@@ -34,9 +42,28 @@ const app = () => {
         </MainPane>
         <RightPane hide="none" />
       </AppBody>
+      <StoryModal />
       <ButtomNav />
     </>
   );
 };
 
 export default app;
+
+const ProgressIndicator = () => {
+  const [windowWidth, setWindowWidth] = React.useState(0);
+  //set window with in useEffect
+  React.useEffect(() => {
+    setWindowWidth(window.innerWidth);
+  }, []);
+  const { scrollYProgress } = useViewportScroll();
+  const yRange = useTransform(scrollYProgress, [0, 0.9], [0, windowWidth]);
+  const pathLength = useSpring(yRange, { stiffness: 400, damping: 90 });
+  console.log(scrollYProgress);
+  return (
+    <motion.div
+      className="fixed top-0 left-0 z-40 h-1 bg-primaryColor"
+      style={{ width: pathLength }}
+    ></motion.div>
+  );
+};
