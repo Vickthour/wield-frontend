ff966d6f4267b5fe0cf7e60487786900ae39bc40
Code-Victor <oluwaborihamzat@gmail.com>
2022-07-24 07:12:05 +0100

    adding Wallet Card

--- components/inc/RightPane.tsx
+++ components/inc/RightPane.tsx
@@ -2,11 +2,13 @@ import React, { useState,useEffect } from "react";
 
 import Activities from "./Activities";
 import Suggestion from "./Suggestion";
+import WalletCard from "./WalletCard";
 
 const RightPane = ({ hide }: { hide: "activities" | "suggestion"|"none" }) => {
   return (
     <div className="col-span-3 hidden flex-1 md:block">
       <div className="sticky top-[90px] space-y-4">
+        <WalletCard />
         {hide === "activities" ? null : (
           <Activities/>
         )}
