893a27bef5e8a46d0e6a0c0f6d14077a2243f1f6
Code-Victor <oluwaborihamzat@gmail.com>
2022-08-02 22:25:08 +0100

    exported newly created components

--- components/base/index.tsx
+++ components/base/index.tsx
@@ -5,3 +5,5 @@ export { default as IconButton } from "./IconButton";
 export { default as Card } from "./Card";
 export { default as Avatar } from "./Avatar";
 export { default as Button } from "./Button";
+export { default as Modal } from "./Modal";
+export { default as Switch } from "./Switch";
