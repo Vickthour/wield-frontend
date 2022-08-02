44a16ba95673ba21b412e8e57418a2b4b060dcb3
Code-Victor <oluwaborihamzat@gmail.com>
2022-08-02 22:26:46 +0100

    added a new Icon

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -866,3 +866,24 @@ export const ArrowLeft = ({ size = "original", className }: iconProp) => {
     </svg>
   );
 };
+// 38
+export const ArrowAngled = ({ size = "original", className }: iconProp) => {
+  return (
+    <svg
+      className={`${sizes[size]} ${className ? className : ""}`}
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.66667 1H11V9.33333M11 1L1 11L11 1Z"
+        stroke="#2A2A2A"
+        strokeWidth="1.66667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
