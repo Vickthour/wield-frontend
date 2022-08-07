f35de3e8207e6ba8f5b0b0ec800aa51ce4de8f9d
Code-Victor <oluwaborihamzat@gmail.com>
2022-08-07 22:12:02 +0100

    formated file

--- components/base/Card.tsx
+++ components/base/Card.tsx
@@ -1,43 +1,44 @@
-import { variantProps, VariantPropsOf } from 'classname-variants/react'
-import React, { FC } from 'react'
+import { variantProps, VariantPropsOf } from "classname-variants/react";
+import React, { FC } from "react";
 
-const tw= String.raw
-const CardClassNames=variantProps({
-    base:tw`bg-white rounded-[10px]`,
-    variants:{
-        shadow:{
-            true:tw`shadow-md`
-        },
-        px:{
-            none:'px-0',
-            sm:tw`px-3`,
-            md:tw`px-5`,
-            lg:tw`px-6`,
-            xl:tw`px-10`
-        },
-        py:{
-            none:tw`py-0`,
-            sm:tw`py-2`,
-            md:tw`py-3`,
-            lg:tw`py-5`,
-            xl:tw`py-10`,
-        },
+const tw = String.raw;
+const CardClassNames = variantProps({
+  base: tw`bg-white rounded-[10px]`,
+  variants: {
+    shadow: {
+      true: tw`shadow-md`,
     },
-    defaultVariants:{
-        px:'md',
-        py:'md'
-    }
-})
+    px: {
+      none: "px-0",
+      sm: tw`px-3`,
+      md: tw`px-5`,
+      lg: tw`px-6`,
+      xl: tw`px-10`,
+    },
+    py: {
+      none: tw`py-0`,
+      sm: tw`py-2`,
+      md: tw`py-3`,
+      lg: tw`py-5`,
+      xl: tw`py-10`,
+    },
+  },
+  defaultVariants: {
+    px: "md",
+    py: "md",
+    shadow: false,
+  },
+});
 type cardProps = JSX.IntrinsicElements["div"] &
-  VariantPropsOf<typeof CardClassNames>
+  VariantPropsOf<typeof CardClassNames>;
 
-const Card:FC<cardProps> = ({className,children,...props}) => {
-    const {className:variantClasses,...otherProps}=CardClassNames(props)
+const Card: FC<cardProps> = ({ className, children, ...props }) => {
+  const { className: variantClasses, ...otherProps } = CardClassNames(props);
   return (
-    <div className={`${className} ${variantClasses}`} {...otherProps}>
-        {children}
+    <div className={`${className ?? ""} ${variantClasses}`} {...otherProps}>
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
