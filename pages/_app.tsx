177c7a8ef6768557683c294bb836302e3edd208d
callmehalpha <hello@callmehalpha.me>
2022-08-08 01:19:45 +0100

    updated the font family to be just roboto font
    Took 1 hour 6 minutes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { ReactElement, ReactNode, useMemo, useState, useEffect } from "react";
 import React from "react";
 import * as portals from "react-reverse-portal";
 import modalContext from "../components/context/modalContext";
+import Head from "next/head";
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -41,6 +42,13 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
               type="text/javascript"
               src="/js/hs-ui.bundle.js"
             ></Script>
+            <Head>
+              <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+                  <link
+                      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
+                      rel="stylesheet"/>
+            </Head>
             <Component {...pageProps} />
           </>
         )}
